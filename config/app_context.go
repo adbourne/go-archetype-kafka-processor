4f337c75bf1d9670e10bf043ee2a3238780b857d
Aaron Bourne <contact@aaronbourne.co.uk>
2017-07-04 18:57:12 +0100

    Formatted config/app_context.go with gofmt

--- config/app_context.go
+++ config/app_context.go
@@ -7,17 +7,17 @@ import (
 // AppContext is the application context
 type AppContext struct {
 	// AppConfig is the application config
-	AppConfig           AppConfig
+	AppConfig AppConfig
 
 	// RandomNumberService is the random number service
 	RandomNumberService services.RandomNumberService
 
 	// KafkaClient is the Kafka client
-	KafkaClient         KafkaClient
+	KafkaClient KafkaClient
 
 	// KafkaProcessor is the Kafka Processor
-	KafkaProcessor      KafkaProcessor
+	KafkaProcessor KafkaProcessor
 
 	// HTTPServer is the HTTP server
-	HTTPServer          HTTPServer
+	HTTPServer HTTPServer
 }
