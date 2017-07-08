b7f7d4d8b5f0b9733f7794efecdf2920e31d067d
Aaron Bourne <contact@aaronbourne.co.uk>
2017-07-08 08:48:20 +0100

    Fix import issue

--- services/http_server_service_test.go
+++ services/http_server_service_test.go
@@ -2,11 +2,10 @@ package services
 
 import (
 	"testing"
-	"github.com/adbourne/go-archetype-rest/config"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewDefaultHTTPServerReturnsNewHTTPServer(t *testing.T) {
-	result := NewDefaultHTTPServer(1234, config.NewLogger())
+	result := NewDefaultHTTPServer(1234, NewSystemOutLogger())
 	assert.NotNil(t, result)
 }
